b7a778cb7c467c71f0b1c08fd7d82df3b6d560b7
tluanga <tluanga@live.com>
2024-01-25 23:49:57 +0530

    all test passed

--- features/item/item/tests.py
+++ features/item/item/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from features.base.base_test_setup_class import BaseTestCase
+from features.id_manager.models import IdManager
 from features.item.item_category.models import ItemCategory
 
 from features.item.unit_of_measurement.models import UnitOfMeasurement
@@ -21,9 +22,61 @@ class ItemModelTestCase(BaseTestCase):
             is_active=True
         )
        
-    def test_item_creation(self):
+    def test_create_item(self):
+        Item.objects.all().delete()
+        item = Item.objects.create(
+            name='Test Item',
+            description='This is a test item',
+            type=self.item_type,
+            unit_of_measurement=self.unit_of_measurement
+        )
         self.assertEqual(Item.objects.count(), 1)
-        self.assertEqual(Item.objects.get().name, "Test Item")
+        self.assertEqual(item.name, 'Test Item')
+        self.assertEqual(item.description, 'This is a test item')
+        self.assertEqual(item.type, self.item_type)
+        self.assertEqual(item.unit_of_measurement, self.unit_of_measurement)
+        self.assertTrue(item.is_active)
+
+    def test_item_code_generation(self):
+        IdManager.objects.all().delete()
+        item = Item.objects.create(
+            name='Test Item',
+            description='This is a test item',
+            type=self.item_type,
+            unit_of_measurement=self.unit_of_measurement
+        )
+        alpha_part = item.item_code.rstrip('0123456789')
+        num_part = item.item_code[len(alpha_part):]
+
+        # Increment the numeric part
+        new_num = str(int(num_part) + 1).zfill(len(num_part))
+
+        # Return the incremented string
+        new_code= alpha_part + new_num
+        self.assertIsNotNone(item.item_code)
+        self.assertEqual(new_code, IdManager.generateId(self.item_type.category.abbreviation))
+
+    def test_item_update(self):
+        item = Item.objects.create(
+            name='Test Item',
+            description='This is a test item',
+            type=self.item_type,
+            unit_of_measurement=self.unit_of_measurement
+        )
+        item.name = 'Updated Item'
+        item.save()
+        self.assertEqual(Item.objects.get(id=item.id).name, 'Updated Item')
+
+    def test_item_deletion(self):
+        item = Item.objects.create(
+            name='Test Item',
+            description='This is a test item',
+            type=self.item_type,
+            unit_of_measurement=self.unit_of_measurement
+        )
+        item_id = item.id
+        item.delete()
+        self.assertFalse(Item.objects.filter(id=item_id).exists())
 
 
 class ItemSerializerTestCase(BaseTestCase):
