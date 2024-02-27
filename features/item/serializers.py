c6af0f3175f174fb0416c75c09ff8f4d1e8290b5
tluanga <tluanga@live.com>
2024-02-27 16:23:38 +0530

    a

--- features/item/serializers.py
+++ features/item/serializers.py
@@ -73,7 +73,7 @@ class ItemBatchSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ItemBatch
-        fields = ['batch_id', 'description', 'date_of_expiry', 'item', 'is_active', 'created_on', 'updated_on']
+        fields = ['id','batch_id', 'description', 'date_of_expiry', 'item', 'is_active', 'created_on', 'updated_on']
 
 
 
