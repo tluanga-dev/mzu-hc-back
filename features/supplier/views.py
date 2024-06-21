02daf040609c21f6d52e9e630a29ced5acbae1fb
tluanga <tluanga@live.com>
2024-06-21 21:18:35 +0530

    a

--- features/supplier/views.py
+++ features/supplier/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+
+from features.item.views import StandardResultsSetPagination
 from .models import Supplier
 from .serializers import SupplierSerializer
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    pagination_class = StandardResultsSetPagination
